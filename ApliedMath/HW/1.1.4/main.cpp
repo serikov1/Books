d596a4bbe1599a59c8aa10b82b047083779f49a3
serikov vasilii <90526810+serikov1@users.noreply.github.com>
2023-10-11 07:12:43 +0300

    upd

---
+++ ApliedMath/HW/1.1.4/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+double f1(double xn, double yn) {
+    return pow(xn, 2) + pow(yn,2) - 1;
+}
+
+double f2(double xn, double yn) {
+    return yn - tan(xn);
+}
+
+double det(double x, double y) {
+    return  1/( 2*x + 2*y/(pow(cos(x), 2)) );
+}
+
+std::vector<double> JF(std::vector<double> Xn) {
+    return {det(Xn[0], Xn[1])*( f1(Xn[0], Xn[1])*2*Xn[0] - 2*Xn[1]*f2(Xn[0], Xn[1]) ),
+            det(Xn[0], Xn[1])*( f1(Xn[0], Xn[1])/ pow(cos(Xn[0]), 2) + 2*Xn[1]*f2(Xn[0], Xn[1])) };
+}
+
+
+int main() {
+    std::vector<double> Xn = {-0.64, -0.76};
+    std::vector<double> Xn1 = {0, 0};
+
+    double epsilon = 0.000001;
+    uint32_t n = 1;
+    while (std::abs(Xn1[0] - Xn[0]) > epsilon && std::abs(Xn1[1] - Xn[1]) > epsilon) {
+        if(n > 1) {
+            Xn[0] = Xn1[0];
+            Xn[1] = Xn1[1];
+        }
+        Xn1[0] = Xn[0] - JF(Xn)[0];
+        Xn1[1] = Xn[1] - JF(Xn)[1];
+        n++;
+    }
+
+    std::cout<<"Первый нуль функции: " << "x = " << Xn1[0]<< ";" << " y = " << Xn1[1] << "\n" << "Получен на " << n << " шаге МПИ \n" << std::endl;
+
+
+    Xn = {0.7, 0.7};
+    Xn1 = {0, 0};
+
+    n = 1;
+    while (std::abs(Xn1[0] - Xn[0]) > epsilon && std::abs(Xn1[1] - Xn[1]) > epsilon) {
+        if(n > 1) {
+            Xn[0] = Xn1[0];
+            Xn[1] = Xn1[1];
+        }
+        Xn1[0] = Xn[0] - JF(Xn)[0];
+        Xn1[1] = Xn[1] - JF(Xn)[1];
+        n++;
+    }
+
+    std::cout<<"Второй нуль функции: " << "x = " << Xn1[0]<< ";" << " y = " << Xn1[1] << "\n" << "Получен на " << n << " шаге МПИ \n" << std::endl;
+
+}
