f99768b009ac58ee77c3988cee33850ab1202afc
serikov.vr <vasilij.serikov.03@mail.ru>
2023-10-21 12:44:03 +0300

    upd

---
+++ AppliedMath/HW/1.1.3/cmake-build-debug/CMakeFiles/1_1_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_1_2.exe"
+  "1_1_2.exe.manifest"
+  "1_1_2.lib"
+  "1_1_2.pdb"
+  "CMakeFiles/1_1_2.dir/newton.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_1_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AppliedMath/HW/1.1.3/cmake-build-debug/CMakeFiles/second.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/second.dir/spline.cpp.obj"
+  "second.exe"
+  "second.exe.manifest"
+  "second.lib"
+  "second.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/second.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
